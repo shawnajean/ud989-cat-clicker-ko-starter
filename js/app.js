5f257e1f0ca7c8e19244f70b949099f586130684
shawnajean <shawnajean@users.noreply.github.com>
2016-02-06 15:26:01 -0600

    add multiple cats

--- js/app.js
+++ js/app.js
@@ -1,3 +1,37 @@
+var initialCats = [
+  {
+    name: "Xuxa",
+    imgSrc: "https://farm2.staticflickr.com/1126/625069434_db86b67df8_b.jpg",
+    imgAttribution: "https://flic.kr/p/XeDnd",
+    nicknames: [ 'Xu' ],
+    clickCount: 0
+  },{
+    name: "Mask",
+    imgSrc: "https://farm4.staticflickr.com/3684/9860937706_8608be2950_b.jpg",
+    imgAttribution: "https://flic.kr/p/g2nTem",
+    nicknames: [ 'Em' ],
+    clickCount: 0
+  },{
+    name: "Sunny",
+    imgSrc: "https://farm8.staticflickr.com/7401/16393044637_72e93d96b6_b.jpg",
+    imgAttribution: "https://flic.kr/p/qYAD3D",
+    nicknames: [ 'Sun' ],
+    clickCount: 0
+  },{
+    name: "Yoann",
+    imgSrc: "https://farm6.staticflickr.com/5704/20443802614_d0b0c0b5c5_b.jpg",
+    imgAttribution: "https://flic.kr/p/x9xQgq",
+    nicknames: [ 'Yo' ],
+    clickCount: 0
+  },{
+    name: 'Turkish',
+    imgSrc: 'https://farm8.staticflickr.com/7160/6672148713_4e2ab31099_b.jpg',
+    imgAttribution: 'https://flic.kr/p/baAvmM',
+    nicknames: [ 'Turk', 'Turkey', 'Mr T' ],
+    clickCount: 0
+  }
+];
+
 var Cat = function( data ) {
   this.clickCount = ko.observable( data.clickCount );
   this.name = ko.observable( data.name );
@@ -7,37 +41,41 @@ var Cat = function( data ) {
   this.nicknames = ko.observableArray( data.nicknames );
 
   this.level = ko.computed( function() {
-      if( this.clickCount() < 10 ) {
-        return "Newborn";
-      } else if( this.clickCount() < 100 ) {
-        return "Infant";
-      } else if( this.clickCount() < 250 ) {
-        return "Teen";
-      } else if( this.clickCount() < 750 ) {
-        return "Young adult";
-      } else if( this.clickCount() < 1500 ) {
-        return "Adult";
-      } else {
-        return "Retired";
-      }
+    var currentClicks = this.clickCount();
+    if( currentClicks < 10 ) {
+      return "Newborn";
+    } else if( currentClicks < 100 ) {
+      return "Infant";
+    } else if( currentClicks < 250 ) {
+      return "Teen";
+    } else if( currentClicks < 750 ) {
+      return "Young adult";
+    } else if( currentClicks < 1500 ) {
+      return "Adult";
+    } else {
+      return "Retired";
+    }
   }, this);
-}
+};
 
 var ViewModel = function() {
   var self = this;
 
-  this.currentCat = ko.observable( new Cat({
-    clickCount: 0,
-    name: 'Turkish',
-    imgSrc: 'https://farm8.staticflickr.com/7160/6672148713_4e2ab31099_b.jpg',
-    imgAttribution: 'https://flic.kr/p/baAvmM',
-    nicknames: [ 'Turk', 'Turkey', 'Mr T' ]
-  }) );
+  this.catList = ko.observableArray([]);
 
-  // function is called from within the binding context of currentCat from HTML
+  initialCats.forEach( function( catItem ) {
+    self.catList.push( new Cat( catItem ) );
+  });
+
+  this.currentCat = ko.observable( this.catList()[0] );
+
   this.incrementCounter = function() {
     self.currentCat().clickCount( self.currentCat().clickCount() + 1 );
   }
-}
+
+  this.selectCat = function( clickedCat ) {
+    self.currentCat( clickedCat );
+  }
+};
 
 ko.applyBindings( new ViewModel() );
